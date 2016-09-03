8d32a3a17c071ea126d2bedad33390afe8317140
Nicolas Eckhart <nicolas.eckhart@renuo.ch>
2016-09-04 00:33:31 +0200

    added stopwatch helper

---
+++ helpers/stopwatch.rb
@@ -0,0 +1,17 @@
+class Stopwatch
+  attr_accessor :start_time
+
+  def time_box_block
+    @start_time = Time.now
+    yield
+    stop
+  end
+
+  def start
+    @start_time = Time.now
+  end
+
+  def stop
+    puts "\n\nPROGRAM TOOK #{Time.now - @start_time} SECONDS TO RUN\n\n"
+  end
+end
